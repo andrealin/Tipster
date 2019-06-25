f3c34743110c2e1c4c5cbbf1bd445dc17a8f8736
Andrea Lin <drealin@fb.com>
2019-06-25 14:00:05 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by drealin on 6/25/19.
+//  Copyright © 2019 drealin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tipster/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tipster
+//
+//  Created by drealin on 6/25/19.
+//  Copyright © 2019 drealin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
