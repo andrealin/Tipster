b9e19c05c4397531f0f47a6d66d1516764bdbf4c
Andrea Lin <drealin@fb.com>
2019-06-25 17:51:04 -0700

    create empty settings page and show it using a button

---
+++ Tipster/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  Tipster
+//
+//  Created by drealin on 6/25/19.
+//  Copyright © 2019 drealin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
